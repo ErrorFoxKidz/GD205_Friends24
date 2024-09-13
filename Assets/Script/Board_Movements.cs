7fe6ce5c67b51588de41e72e25814b2fe440b544
Raven <aguileraraven@gmail.com>
2024-09-12 20:01:53 -0400

    Added Week 3 stuff and more

---
+++ Assets/Script/Board_Movements.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board_Movements : MonoBehaviour
+{
+
+     //public Transform Enemy;
+
+     //Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //startPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+          transform.position += Vector3.left;
+        }
+
+         if (Input.GetKeyDown(KeyCode.S)) {
+          transform.position += Vector3.back;
+        }
+
+         if (Input.GetKeyDown(KeyCode.W)) {
+          transform.position += Vector3.forward;
+
+        }
+
+         if (Input.GetKeyDown(KeyCode.D)) {
+          transform.position += Vector3.right;
+        }
+
+
+    }
+}
